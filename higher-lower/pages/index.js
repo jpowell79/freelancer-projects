32920d07d6ebf7538fc4c1b89dff023e1ba2fa19
Simon Eriksson <dv16sen@cs.umu.se>
2018-09-29 21:59:03 +0200

    Added animated CSS background.

--- higher-lower/pages/index.js
+++ higher-lower/pages/index.js
@@ -7,6 +7,16 @@ class Index extends Component {
         return (
             <Page>
                 <h1>Hello Next, Redux, React, SCSS and Jest setup!</h1>
+                <p>
+                    <span id="fog">A long</span>
+                    <span id="fog-2">long time</span>
+                    <span id="glow">ago,</span>
+                    <span id="light">in a</span>
+                    <span id="stars">galaxy</span>
+                    <span id="stars-2">far</span>
+                    <span id="small-stars">far</span>
+                    <span id="small-stars-2">away…</span>
+                </p>
             </Page>
         )
     }
