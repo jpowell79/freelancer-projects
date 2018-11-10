591b43cf445a3e41a53a3952a8644c9a44aee5fe
Simon Eriksson <dv16sen@cs.umu.se>
2018-11-10 17:37:12 +0100

    Made CSS background and ad styling work better on mobile devices.

--- higher-lower/site-components/AdSidebar.js
+++ higher-lower/site-components/AdSidebar.js
@@ -4,12 +4,12 @@ import {AdContainer} from "./containers/AdContainer";
 export const AdSidebar = () => {
     return (
         <Fragment>
-            <AdContainer className="bg-color-white glass">
+            <AdContainer>
                 <a target="_blank" href="https://binance.com">
                     <img src="/static/images/ads/binance.png"/>
                 </a>
             </AdContainer>
-            <AdContainer className="bg-color-white glass">
+            <AdContainer>
                 <a target="_blank" href="https://coinbase.com">
                     <img src="/static/images/ads/coinbase.png"/>
                 </a>
